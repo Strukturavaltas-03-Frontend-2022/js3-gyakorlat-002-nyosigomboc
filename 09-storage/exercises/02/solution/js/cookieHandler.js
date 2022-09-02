2c097e4b77158a72b902b11c8699cb3f6f705510
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-02 02:20:43 +0200

    done

--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,1 +1,21 @@
+const cookieHandler = {
+  getAll() {
+    return document.cookie.split(';').reduce((prev, curr) => {
+      const [full, key, value] = /^[ ]*([^=]+)=([^;]*)/.exec(curr);
+      prev[key] = value;
+      return prev;
+    }, {});
+  },
+  toSessionStorage() {
+    Object.entries(this.getAll()).forEach(([key, value]) => {
+      sessionStorage.setItem(key, value);
+    });
+  },
+  flush() {
+    Object.keys(this.getAll()).forEach((key) => {
+      document.cookie = `${key}=; expires=${new Date(0).toUTCString()}`;
+    });
+  },
+};
+
 export default cookieHandler;
